3e28d6cabd6587e138a5e11eca12c1a1ec5c0a04
hifest <gtoksteampro2123@gmail.com>
2022-04-13 11:38:20 +0300

    сорян не добавил,но я сделал новую анимацю наведения

--- src/components/gameList/GameList.js
+++ src/components/gameList/GameList.js
@@ -8,34 +8,37 @@ function GameList() {
   const dispatch = useDispatch();
   const { games, gamesLoadingStatus } = useSelector((state) => state.games);
   useEffect(() => {
-    dispatch(fetchGamesList(12)); // 12 - скільки ігор хочеш отримати,не води 69 бо напросишся
+    dispatch(fetchGamesList(6)); // 12 - скільки ігор хочеш отримати,не води 69 бо напросишся
   }, []);
 
   const renderGames = (arr) => {
     if (!arr) {
-      return (
-        <h5 >
-          Ігри не найдені, перезагрузіть сторінку!
-        </h5>
-      );
+      return <h5>Ігри не найдені, перезагрузіть сторінку!</h5>;
     }
     return arr.map((item) => {
-      return( 
-        <div key={item.name}> 
+      return (
+        <div key={item.name}>
           <img src={item.background_image}></img>
-        {item.name} 
-        <p className='t-a-l'>
-          Рейтинг: {item.rating} <br />
-          Играть ч. : {item.playtime}
-        </p>
+          {item.name}
+            <div className="t-a-l">
+              Рейтинг: {item.rating}
+            </div>
         </div>
-      )
+      );
     });
   };
-  return( 
+  return (
     <div className="container">
       <div className="gameList">
-        <div className="gameList__games">  { gamesLoadingStatus == 'loading' ? <div> Loading! </div> : gamesLoadingStatus == 'error' ? <h5 className="text-center mt-5"> Ошибка загрузки </h5> : renderGames(games.results)} </div>
+        <div className="gameList__games">
+          {gamesLoadingStatus == "loading" ? (
+            <div> Loading! </div>
+          ) : gamesLoadingStatus == "error" ? (
+            <h5 className="text-center mt-5"> Ошибка загрузки </h5>
+          ) : (
+            renderGames(games.results)
+          )}
+        </div>
         {/* / Цей коментар все пояснює. */}
       </div>
     </div>
