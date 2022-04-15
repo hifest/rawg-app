58d8e70b850428f4d49e52fb51ca35371650ec44
hifest <gtoksteampro2123@gmail.com>
2022-04-15 12:55:15 +0300

    i fix pizda

--- src/components/gameList/gameListSlice.js
+++ src/components/gameList/gameListSlice.js
@@ -10,11 +10,10 @@ const initialState = {
 export const fetchGamesList = createAsyncThunk(
 	'games/fetchGamesList',
 	async (page_size,activeFilter) => {
-		console.log(activeFilter.getState());
-		let aaa = activeFilter.getState()
+		let filter = activeFilter.getState().games.activeFilter
 		const { request } = useHttp()
 		return await request(
-			`https://api.rawg.io/api/games?key=e2f90b4e56164fc6996b2abb0faa856e&genres=${aaa.searchGames.activeFilter}&page_size=${page_size}`
+			`https://api.rawg.io/api/games?key=e2f90b4e56164fc6996b2abb0faa856e&genres=${filter}&page_size=${page_size}`
 		)
 	}
 )
